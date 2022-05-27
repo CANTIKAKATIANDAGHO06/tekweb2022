ae369ca1a600821d9da1bc9d7f9fe132eb07ff9c
CANTIKAKATIANDAGHO06 <103124539+CANTIKAKATIANDAGHO06@users.noreply.github.com>
2022-05-28 00:28:49 +0700

    Update main.js

--- main.js
+++ main.js
@@ -10,44 +10,41 @@ Vue.createApp({
       };
   },
   methods: {
-      getArticles() {
-          axios
-              .get(
-                  "https://my-json-server.typicode.com/CANTIKAKATIANDAGHO06/tekweb2022/article"
-              )
-              .then((res) => {
-                  this.articles = res.data;
-              })
-              .catch((error) => {
-                  console.log(error);
-              });
-      },
-      getMarkdown() {
-          const queryString = window.location.search;
-          const urlParams = new URLSearchParams(queryString);
-          const articles = urlParams.get('Article');
-          var converter = new showdown.Converter();
-          console.log(articles);
-          axios
-              .get(
-                  src = "https://raw.githubusercontent.com/CANTIKAKATIANDAGHO06/tekweb2022/new/article/" + articles
-              )
-              .then((res) => {
-                  var html = converter.makeHtml(res.data);
-                  this.article = html;
-                  console.log(html);
-              })
-              .catch((error) => {
-                  console.log(error);
-              });
-      }
-  },
+    getArticles() {
+        axios
+            .get(
+                "https://my-json-server.typicode.com/CANTIKAKATIANDAGHO06/tekweb2022/article"
+            )
+            .then((res) => {
+                this.articles = res.data;
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    },
+    getMarkdown() {
+        const queryString = window.location.search;
+        const urlParams = new URLSearchParams(queryString);
+        const articles = urlParams.get('article');
+        var converter = new showdown.Converter();
+        console.log(articles);
+        axios
+            .get(
+                src = "https://raw.githubusercontent.com/CANTIKAKATIANDAGHO06/tekweb2022/new/article/" + articles
+            )
+            .then((res) => {
+                var html = converter.makeHtml(res.data);
+                this.article = html;
+                console.log(html);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+},
 
-  beforeMount() {
-          this.getArticles(),
-          this.getMarkdown()
-  }
+beforeMount() {
+        this.getArticles(),
+        this.getMarkdown()
+}
 }).mount("#app");
-  
-  
-  
